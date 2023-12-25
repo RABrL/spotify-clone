e3c37e18a3849e46f2d6ef4143697da737db2ec1
RABrL <robin_brito@hotmail.com>
2023-12-24 21:10:15 -0500

    Add MediaItem, PlayButton, and SongItem components

---
+++ components/MediaItem.tsx
@@ -0,0 +1,66 @@
+import useLoadImage from '@/hooks/useLoadImage'
+import { Song } from '@/types'
+import Image from 'next/image'
+
+interface MediaItemProps {
+  song: Song
+  onClick: (id: number) => void
+}
+
+const MediaItem = ({ onClick, song }: MediaItemProps) => {
+  const imageUrl = useLoadImage(song)
+
+  const handleClick = () => {
+    if (onClick) {
+      return onClick(song.id)
+    }
+
+    // Default turn on player
+  }
+  return (
+    <button
+      onClick={handleClick}
+      className="
+      flex
+      items-center
+      gap-x-3
+      cursor-pointer
+      hover:bg-neutral-800/50
+      w-full
+      p-2
+      rounded-md
+    "
+    >
+      <div
+        className="
+        relative
+        rounded-md
+        min-h-[48px]
+        min-w-[48px]
+        overflow-hidden
+      "
+      >
+        <Image
+          fill
+          src={imageUrl || '/images/liked.png'}
+          alt={`Cover for ${song.title}`}
+          className="object-cover"
+        />
+      </div>
+      <div
+        className="
+          flex
+          flex-col
+          gap-y-1
+          overflow-hidden
+          items-start
+        "
+      >
+        <p className="text-white truncate">{song.title}</p>
+        <p className='text-neutral-400 text-sm truncate'>{song.author}</p>
+      </div>
+    </button>
+  )
+}
+
+export default MediaItem
---
+++ components/SongItem.tsx
@@ -0,0 +1,90 @@
+'use client'
+
+import Image from 'next/image'
+
+import useLoadImage from '@/hooks/useLoadImage'
+import { Song } from '@/types'
+
+import PlayButton from './PlayButton'
+
+interface SongItemProps {
+  song: Song
+  onClick: (id: number) => void
+}
+
+const SongItem = ({ song, onClick }: SongItemProps) => {
+  const imagePath = useLoadImage(song)
+  return (
+    <button
+      onClick={() => onClick(song.id)}
+      className="
+        relative
+        group
+        flex
+        flex-col
+        items-center
+        justify-center
+        rounded-md
+        overflow-hidden
+        gap-x-4
+        bg-neutral-400/5
+        cursor-pointer
+        hover:bg-neutral-400/10
+        transition
+        p-3
+      "
+    >
+      <div
+        className="
+          relative
+          aspect-square
+          w-full
+          h-full
+          rounded-md
+          overflow-hidden
+        "
+      >
+        <Image
+          className="object-cover"
+          src={imagePath || '/images/liked.png'}
+          fill
+          alt={`Cover of ${song.title}`}
+        />
+      </div>
+      <div
+        className="
+          flex
+          flex-col
+          w-full
+          pt-4
+          gap-y-1
+          text-start
+        "
+      >
+        <p className="font-semibold truncate w-full">{song.title}</p>
+        <p
+          className="
+            text-neutral-400
+            text-sm
+            pb-4
+            w-full
+            truncate
+          "
+        >
+          By {song.author}
+        </p>
+      </div>
+      <div
+        className="
+          absolute
+          bottom-24
+          right-5
+        "
+      >
+        <PlayButton />
+      </div>
+    </button>
+  )
+}
+
+export default SongItem
